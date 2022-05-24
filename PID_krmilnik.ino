c7890fbeedd1fbc2d32b1534fa46635b0393c489
Sabru <69241571+Sabru-soru@users.noreply.github.com>
2022-05-24 15:27:36 +0200

    Add files via upload

---
+++ PID_krmilnik.ino
@@ -0,0 +1,106 @@
+/* Program za krmiljenje DC motorja. DKS - 2016 , tim9 */
+
+#define F_CPU 16000000    //dolocimo frekvenco ure za fast PWM
+
+int PWM_pin = 6;          //pin ki je povezan na tranzistor
+int enk1 = 2;             //prvi enkoder
+int enk2 = 3;             //drugi enkoder
+int trig=11;              //pin triggerja, s katerim zacnemo zajemanje na osciloskopu
+int pwm =0;               //začetna vrednost PWM (korekcija)
+int setpnt=0;             //to je naša zeljena vrednost, ki jo bomo vnasali prek serijske komunikacije
+int napaka=0;             //zacetna napaka  
+int vsota=0;              //vsota napak, ki bo uporabljena za integrirni del
+int prejnapaka=0;         //prejsna napaka za diferencirni del
+int korekcija=0;          //z vrednostjo korekcije krmilimo PWM
+int a=0;                  //stevec, da se vrednosti na serial monitorju izpisujejo pocasneje
+int prejsetpnt=0;         //prejsna nastavljena vrednost. Pomembno za trigger
+int prejkorekcija=0;      
+double gred=0;
+
+volatile unsigned long casK=0;
+volatile unsigned long casZ=0;
+volatile unsigned long perioda;
+volatile unsigned long frekvenca=0;
+
+double Kp=0.102;          //vrednost proporcionalnega ojacenja
+double Ki=0.02;           //vrednost integrirnega ojacenja
+double Kd=0;              //vrednost diferencirnega ojacenja
+
+void setup() {
+
+  //attachInterrupt(digitalPinToInterrupt(enk1),prekinitev,CHANGE); //Prvi enkoder ni delal pravilno, zato ga nismo vkljucili v program
+  attachInterrupt(digitalPinToInterrupt(enk2),prekinitev,RISING);   //Preracun frekvence se zgodi na rising edge pulza enkoderja
+
+  //inicializacija serijske komunikacije
+  Serial.begin(9600); 
+  Serial.println("Program za krmiljenje DC motorja."); 
+  Serial.println("Vnesi frekvenco: ");
+
+
+  TCCR0A|=(1<<WGM01)|(1<<WGM00);      //nastavimo registre za Fast PWM
+  TCCR0B|=(1<<CS01);                  //nastavimo prescaler na 8 s katerim delimo frekvenco ure
+  OCR0A=0;                            //nastavimo zacetno vrednost PWM - output compare register A 0/255
+  DDRD|=(1<<PD6);                     //nastavimo izhod na pin6 - 0c0A pin - output-PD6
+  TCCR0A|=(1<<COM0A1);                //Clear 0C0A on compare match
+
+  delay(100);
+  pinMode(trig,OUTPUT);
+  digitalWrite(trig,LOW);
+}
+
+void loop() {
+  
+    prejsetpnt=setpnt;                //to potrebujemo za prozenje triggerja
+    napaka = (setpnt - frekvenca);    //tu se izracuna razlika med zeljeno in dejansko vrtilno frekvenco
+    vsota+=napaka;                    
+    korekcija=(napaka*Kp)+vsota*Ki+(napaka-prejnapaka)*Kd;                //glavni izracun korekcije
+    //korekcija= prejkorekcija+27/256 *(napaka - 252/256*prejnapaka);     //poskus implementacije krmilnika iz Sisotool
+    prejnapaka=napaka
+    prejkorekcija=korekcija;
+    
+    if((korekcija)>=255){             //ce je korekcija vec kot 255 omejimo PWM na najvisjo vrednost
+      korekcija=255; 
+    }
+    else if((korekcija)<0){           //ce je korekcija negativna nastavimo PWM na nic
+      korekcija=0; 
+    }
+    
+  OCR0A=korekcija;                    //Registru za PWM pripisemo vrednost korekcije
+  gred=frekvenca/9.68;                //Na izhodni gredi je se reduktor, ki zmanjsa hitrost za 9,68
+    
+   if(a==1000){                       //tukaj bo potekal izpis podatkov, s katerimi si pomagamo
+     Serial.print("Napaka: ");       
+     Serial.print(napaka);
+     Serial.print("\t Korak: ");
+     Serial.print(korekcija);
+     Serial.print("\t: PWM: ");
+     Serial.print(OCR0A,DEC);
+     Serial.print("\t Period: ");
+     Serial.print(perioda,DEC);
+     Serial.print("\t Frekv: ");
+     Serial.print(frekvenca,DEC);
+     Serial.print("\t FrekvGr: ");
+     Serial.println(gred,2);
+     a=0;                             //spremenljivko postavimo na 0, da se zopet zacne stetje
+     digitalWrite(trig,LOW);          //trigger postavimo na nizko stanje
+   }
+   a++;
+
+// Beremo serijski port - nastavitev hitrosti
+  while(Serial.available() > 0)
+  {
+    setpnt = Serial.parseInt();       //preberemo nastavljeno vrednost
+    Serial.println(setpnt);
+    if(prejsetpnt!=setpnt){           //tukaj zazna, ce smo zamenjali zeljeno vrednost, takrat prozi triger in zacne zajemati
+      digitalWrite(trig,HIGH);        
+    }
+  }
+}
+
+void prekinitev()                     //tu se bo izracunala frekvenca vrtenja gredi
+{
+  casK=micros();                      //zabelezi se cas od zagona programa v mikrosekundah
+  perioda=casK-casZ;                  //izracuna se perioda med posamezimi pulzi enkoderja
+  casZ=casK;                          //shranimo čas za naslednji cikel
+  frekvenca=(1000000/perioda)/12;     //frekvenca gredi enkoderja v Hz, delimo z 12, ker da enkoder 12 pulzov na obrat
+}
---
+++ nadziranjeProcesov.ino
@@ -0,0 +1,365 @@
+/*Uroš Urbas & Blaž Močan
+ * Gmail account: nadziranjeslike@gmail.com
+ * password:      senzorjiinaktuatorji
+*/
+
+#include <SoftwareSerial.h>
+#include <SD.h>
+File dataFile;
+#define power 4
+#include <Wire.h>
+#include <ArduCAM.h>
+#include <SPI.h>
+#include "memorysaver.h"
+unsigned long tz=0;
+unsigned long tk=0;
+unsigned long sek=300;
+
+SoftwareSerial mySerial(5, 6);        //5 Rx rumena, 6 Tx črna
+#define SD_CS 9                       //SD kartica
+#define SPI_CS 10                     //kamera
+ArduCAM myCAM(OV2640,SPI_CS);
+boolean isShowFlag = true;
+char inc=0;
+
+void setup()
+{ 
+  mySerial.begin(57600);              //Baud rate GSM modula
+  
+  uint8_t vid,pid;                    //za kamero
+  uint8_t temp;
+  #if defined(__SAM3X8E__)
+  Wire1.begin();
+  #else
+  Wire.begin();
+  #endif 
+  
+  Serial.begin(115200);               // Setting the baud rate of Serial Monitor (Arduino)
+  pinMode(3,INPUT);
+  pinMode(power,OUTPUT);
+  pinMode(2,OUTPUT);                  //sluzi za priziganje ledice, debuggig
+  digitalWrite(2,LOW);
+  
+  Serial.println(F("ArduCAM Start!")); 
+  // set the SPI_CS as an output:
+  pinMode(SPI_CS, OUTPUT);
+
+  // initialize SPI:
+  SPI.begin(); 
+  //Check if the ArduCAM SPI bus is OK
+  myCAM.write_reg(ARDUCHIP_TEST1, 0x55);
+  temp = myCAM.read_reg(ARDUCHIP_TEST1);
+  if(temp != 0x55)
+  {
+    Serial.println(F("SPI interface Error!"));
+    while(1);                         //Ce je problem z SPI komunikacijo se program tu ustavi, ideja, da bi se poslalo SMS da ne dela
+  }
+  //mySerial.print("AT+CMGF=1\r");  // set SMS mode to text
+  //delay(100);
+  //mySerial.print("AT+CNMI=2,2,0,0,0\r");
+  //delay(1000);
+  
+  //Check if the camera module type is OV2640
+  myCAM.rdSensorReg8_8(OV2640_CHIPID_HIGH, &vid);
+  myCAM.rdSensorReg8_8(OV2640_CHIPID_LOW, &pid);
+  if((vid != 0x26) || (pid != 0x42))
+    Serial.println(F("Can't find OV2640 module!"));
+  else
+    Serial.println(F("OV2640 detected"));
+    
+  //Nastavi JPEG format in inicializira kamero     
+  myCAM.set_format(JPEG);
+
+  myCAM.InitCAM();
+  
+  //Initialize SD Card
+  if (!SD.begin(SD_CS)) 
+  {
+    //while (1);    
+    Serial.println(F("SD Card Error"));
+  }
+  else
+    Serial.println(F("SD Card detected!"));
+
+Serial.println(F("s - slikaj, m - mms"));
+}
+
+void loop()
+{
+  if(digitalRead(3)==LOW){
+  digitalWrite(power,HIGH);
+  delay(2000);
+  digitalWrite(power,LOW);
+  }
+  
+  if (Serial.available()>0)
+   switch(Serial.read())
+  {
+    case 's':
+      Slikaj();
+      break;
+    case 'm':
+      sendMMS();
+      break;
+  }
+
+ /* if (mySerial.available()>0)
+   Serial.write(mySerial.read());*/
+
+  tk=millis();
+  if((tk-tz)>(sek*1000)){
+  Slikaj();
+  tz=tk;
+  }
+
+  if(mySerial.available() >0){
+    inc=mySerial.read();          //Get the character from the cellular serial port.
+    if(inc=='#'){                  //Ce prejme # zazene slikanje in posiljanje
+      Slikaj();
+    }
+    else if(inc=='*'){
+      sek=sek+60;
+    }
+    else if(inc=='_'){
+      sek=sek-60;
+      if(sek<180)
+      sek=180;
+    }
+  }
+ 
+}
+
+//+++++++++++++++++++++++++Podfunkcije+++++++++++++++++++++
+
+void sendMMS()
+{
+  int i;
+  unsigned char data=0;
+  digitalWrite(2,HIGH);
+
+ //Nastavitve omrežja ---------------------------
+  Serial.println(F("MMS"));
+  mySerial.println(F("AT+CMMSINIT\r"));
+  delay(1000);
+  if(mySerial.available())
+  {
+    while(mySerial.available()) Serial.write(mySerial.read());
+  }
+  mySerial.println(F("AT+CMMSCURL=\"mmc\"\r"));                 //http://mms.celcom.net.my
+  delay(1000);
+  if(mySerial.available())
+  {
+    while(mySerial.available()) Serial.write(mySerial.read());
+  }
+  mySerial.println(F("AT+CMMSCID=1\r"));
+  delay(1000);
+  if(mySerial.available())
+  {
+    while(mySerial.available()) Serial.write(mySerial.read());
+  }
+  mySerial.println(F("AT+CMMSPROTO=\"80.95.224.46\",9201\r"));  //10.128.1.242\",8080
+  delay(1000);
+  if(mySerial.available())
+  {
+    while(mySerial.available()) Serial.write(mySerial.read());
+  }
+  mySerial.println(F("AT+CMMSSENDCFG=6,3,0,0,2,4\r"));
+  delay(1000);
+  if(mySerial.available())
+  {
+    while(mySerial.available()) Serial.write(mySerial.read());
+  }
+  mySerial.println(F("AT+SAPBR=3,1,\"CONTYPE\",\"GPRS\"\r"));   //MMS
+  delay(1000);
+  if(mySerial.available())
+  {
+    while(mySerial.available()) Serial.write(mySerial.read());
+  }
+  mySerial.println(F("AT+SAPBR=3,1,\"APN\",\"mms.simobil.si\"\r")); //mms.celcom.net.my
+  delay(1000);
+  if(mySerial.available())
+  {
+    while(mySerial.available()) Serial.write(mySerial.read());
+  }
+  mySerial.println(F("AT+SAPBR=1,1\r"));
+  delay(1000);
+  if(mySerial.available())
+  {
+    while(mySerial.available()) Serial.write(mySerial.read());
+  }
+  mySerial.println(F("AT+SAPBR=2,1\r"));
+  delay(1000);
+  if(mySerial.available())
+  {
+    while(mySerial.available()) Serial.write(mySerial.read());
+  }
+  mySerial.println(F("AT+CMMSEDIT=1\r"));
+  delay(1000);
+  if(mySerial.available())
+  {
+    while(mySerial.available()) Serial.write(mySerial.read());
+  }
+  dataFile=SD.open(F("1.jpg"));                                     //pošiljal bo sliko z imenom 1.jpg
+  //mySerial.print("AT+CMMSDOWN=\"PIC\",5809,2000000,\"3.jpg\"\r");
+  mySerial.print(F("AT+CMMSDOWN=\"PIC\","));
+  mySerial.print(dataFile.size());                                  //velikost slike v bytih
+  mySerial.println(F(",2000000,\"1.jpg\"\r"));
+  delay(1000);
+  if(mySerial.available())
+  {
+    while(mySerial.available()) Serial.write(mySerial.read());
+  }
+
+  if(dataFile)
+  {
+    while(dataFile.available())
+    {
+      data=dataFile.read();
+      if(data<0x10) Serial.print("0");
+      Serial.print(data,HEX);
+      i++;
+      if((i%40)==0) Serial.println();
+      mySerial.write(data);
+    }
+    dataFile.close();
+  }  
+  else
+  {
+    Serial.println(F("error opening 1.jpg"));
+  }
+  delay(1000);
+  mySerial.println(F("AT+CMMSRECP=\"nadziranjeslike@gmail.com\"\r"));
+  /*delay(1000);
+  mySerial.println("AT+CMMSBCC=\"luka.selak@fs.uni-lj.si\"\r");     //Skrivni prejemnik
+  delay(1000);*/
+  if(mySerial.available())
+  {
+    while(mySerial.available()) Serial.write(mySerial.read());
+  }
+  mySerial.println(F("AT+CMMSVIEW\r"));
+  delay(2000);
+  if(mySerial.available())
+  {
+    while(mySerial.available()) Serial.write(mySerial.read());
+  }
+  mySerial.println(F("AT+CMMSSEND\r"));
+  delay(2000);
+  if(mySerial.available())
+  {
+    while(mySerial.available()) Serial.write(mySerial.read());
+  }
+  mySerial.println(F("AT+CMMSEDIT=0\r"));
+  delay(2000);
+  if(mySerial.available())
+  {
+    while(mySerial.available()) Serial.write(mySerial.read());
+  }
+  mySerial.println(F("AT+CMMSTERM\r"));
+  delay(2000);
+  if(mySerial.available())
+  {
+    while(mySerial.available()) Serial.write(mySerial.read());
+  }
+  digitalWrite(2,LOW);
+}
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++
+void Slikaj()
+{
+  char str[8];
+  File outFile;
+  byte buf[256];
+  static int i = 0;
+  static int k = 1;
+  static int n = 0;
+  uint8_t temp,temp_last;
+  uint8_t start_capture = 0;
+  int total_time = 0;
+  digitalWrite(2,HIGH);
+  
+  isShowFlag = false;
+  myCAM.InitCAM();
+
+  //myCAM.OV2640_set_JPEG_size(OV2640_640x480);
+  myCAM.OV2640_set_JPEG_size(OV2640_1600x1200);
+  //Wait until buttom released - ce bi uporabljal gumb na modulu
+  while(myCAM.get_bit(ARDUCHIP_TRIG, SHUTTER_MASK));
+  delay(1000);
+  start_capture = 1;
+
+  if(start_capture)
+  {
+    //Flush the FIFO 
+    myCAM.flush_fifo();  
+    //Clear the capture done flag 
+    myCAM.clear_fifo_flag();     
+    //Start capture
+    myCAM.start_capture();
+    Serial.println(F("Start Capture"));     
+  }
+  
+  while(!(myCAM.read_reg(ARDUCHIP_TRIG)& CAP_DONE_MASK)){  //pocaka da se slika
+    delay(10);
+  }
+  
+    Serial.println(F("Capture Done!"));
+    
+    //Construct a file name - sharanjevanje slike
+    //k = k + 1;
+    itoa(k, str, 10); 
+    strcat(str,".jpg");
+    //Open the new file  
+    outFile = SD.open(str,O_WRITE | O_CREAT | O_TRUNC);    
+    if (! outFile) 
+    { 
+      Serial.println(F("open file failed"));
+      return;
+    }
+    total_time = millis();
+    i = 0;
+    temp = myCAM.read_fifo();
+    //Write first image data to buffer
+    buf[i++] = temp;
+
+    //Read JPEG data from FIFO
+    while( (temp != 0xD9) | (temp_last != 0xFF) )
+    {
+      temp_last = temp;
+      temp = myCAM.read_fifo();
+      //Write image data to buffer if not full
+      if(i < 256)
+        buf[i++] = temp;
+      else
+      {
+        //Write 256 bytes image data to file
+        outFile.write(buf,256);
+        i = 0;
+        buf[i++] = temp;
+      }
+    }
+    //Write the remain bytes in the buffer
+    if(i > 0)
+      outFile.write(buf,i);
+
+    //Close the file 
+    outFile.close(); 
+    total_time = millis() - total_time;
+    Serial.print(F("Total time used:"));
+    Serial.print(total_time, DEC);
+    Serial.println(F(" millisecond"));    
+    //Clear the capture done flag 
+    myCAM.clear_fifo_flag();
+    //Clear the start capture flag
+    start_capture = 0;
+    
+    //myCAM.set_format(BMP);
+    //myCAM.InitCAM();
+    isShowFlag = true;
+
+    //Po slikanju izvede funkcijo pošiljanja MMSa
+    digitalWrite(2,LOW);
+    delay(2000);
+    sendMMS();
+    
+}
+
